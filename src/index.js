21a02fb552c34a9fe11751b66fa8801f94e80001
Woshiajuana <zhigang.chen@owulia.com>
2020-11-20 21:17:59 +0800

    subcode

--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@
 const validate = function (source, expect, options) {
 
 
-    // 先判断是否
+    // 先判断是校验一个数组
     const type = typeof source
 
+
+}
+
+
+const core = function (value, uses) {
+
+    uses.forEach((use) => {
+
+        const typeUse = Object.prototype.toString().call(use);
+
+        
+
+    });
 
 }
 
